118a889e18302f4bb7d5502aa7e7a6723219b93b
Daffa <daffamaulana30@gmail.com>
2020-12-03 23:36:09 +0800

    error: Bootstrap does not work in footer

--- footer.php
+++ footer.php
@@ -1,22 +1,41 @@
-<footer id="footer">
-	<div class="container">
-		<div class="row">
-			<di class="col-lg-4">
-					<h4>Sidebar footer</h4>
-					<p>Each Theme should have these or similar styles in its style.css file to be able to display images and captions properly. The exact HTML elements and class and ID values will depend on the structure of the Theme you are using. </p>
-			</di>
-						<di class="col-lg-4">
-					<h4>Sidebar footer</h4>
-					<p>Each Theme should have these or similar styles in its style.css file to be able to display images and captions properly. The exact HTML elements and class and ID values will depend on the structure of the Theme you are using. </p>
-			</di>
-						<di class="col-lg-4">
-					<h4>Sidebar footer</h4>
-					<p>Each Theme should have these or similar styles in its style.css file to be able to display images and captions properly. The exact HTML elements and class and ID values will depend on the structure of the Theme you are using. </p>
-			</di>
-		</div>
-	</div>
-	
-</footer> 
+<footer style="position: fixed; bottom:0;margin-bottom: 0px" class="page-footer font-small blue pt-4">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+        <img style="width:190px" alt="birokrat menulis" src="http://localhost/birokratmenulis/wp-content/uploads/2020/12/second.png" />
+        <p>Birokrat Menulis adalah sebuah gerakan untuk mendorong literasi di
+            kalangan para birokrat. Sebuah wadah dimana para birokrat dan para
+            pemerhati birokrat berkontribusi untuk perbaikan negeri ini. Motto
+            kami, kritis, cerdas, & menginspirasi tanpa batas.</p>
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div style="background-color: #f0a500; color: #393e46; height" class="footer-copyright text-center py-3">
+    <p style="margin-bottom:0px"> © 2021 Birokrat Menulis</p>
+    <div style="">
+      <div>
+        <a href="#">
+          <img style="width: 25px;" alt="facebook" src="http://localhost/birokratmenulis/wp-content/uploads/2020/12/facebook.png" />
+        </a>
+      </div>
+      <div>
+        <a href="#">
+          <img style="width: 25px;" alt="twitter" src="http://localhost/birokratmenulis/wp-content/uploads/2020/12/twitter.png" />
+        </a>
+      </div>
+      <div>
+        <a href="#">
+          <img style="width: 25px;" alt="whatsapp" src="http://localhost/birokratmenulis/wp-content/uploads/2020/12/whatsapp.png" />
+        </a>
+      </div>
+    </div>
+  </div>
+  <!-- Copyright -->
+
+</footer>
 
 <?php wp_footer();?>
 </body>
